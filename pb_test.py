f12637c234c11f37605c7567ca136741a4fb2432
spencerqbrown <49326312+spencerqbrown@users.noreply.github.com>
2021-05-27 14:12:55 -0500

    tested pushing text and image

--- pb_test.py
+++ pb_test.py
@@ -1,3 +1,10 @@
 from pushbullet import Pushbullet
+from util import get_key
 
-pb = Pushbullet()
+pb = Pushbullet(get_key())
+# push = pb.push_note("TEST TITLE 1", "TEST BODY 1")
+
+with open("./test_dir/download.png", "rb") as test_pic:
+    file_data = pb.upload_file(test_pic, "TEST PICTURE 1.png")
+
+push = pb.push_file(**file_data)
---
+++ util.py
@@ -0,0 +1,6 @@
+
+
+def get_key():
+    with open("key.txt") as k:
+        return k.read()
+
